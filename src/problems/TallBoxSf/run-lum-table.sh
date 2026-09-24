0f9e703957c550d5fe28374f73a448e2107ae6a8
Chong-Chong He <chongchonghe99@gmail.com>
2026-09-25 00:47:02 +1000

    add slug-fuv.csv

---
+++ src/problems/TallBoxSf/run-lum-table.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+# luminosity table
+
+../../../scripts/python/slug_luminosity_table_for_quokka.py ./slug-fuv.csv --eV 6 11.2 --eV 11.2 13.6 --m0 3 --nt 70 --nm 64 --slug-path ~/softwares/slug2
+
